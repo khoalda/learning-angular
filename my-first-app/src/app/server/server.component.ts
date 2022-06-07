1d03264953aa0f06bf6475f286f0416282ffcc16
laiducav <khoa20082001@gmail.com>
2022-06-07 10:02:08 +0700

    basic components

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -3,11 +3,16 @@ import { Component } from "@angular/core";
 
 
 @Component({
-    selector: 'app-server',
+    // selector: 'app-server', // normal selector
+    // selector: '[app-server]', // attribute selector
+    selector: '.app-server', // class selector
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
+    id = 10
+    status = "active"
 
+    getStatus = () => this.status
 }
 
 
